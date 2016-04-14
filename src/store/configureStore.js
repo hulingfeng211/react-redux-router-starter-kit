3614694a54f3f1d253c4e26d2fe9ae008873319f
cnfi <mark.chenfl@alibaba-inc.com>
2016-04-14 19:26:08 +0800

    up

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,12 +3,20 @@ import createLogger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
 
+import DevTools from '../containers/DevTools';
+
 export default function configureStore(initialState) {
   // Note: only Redux >= 3.1.0 supports passing enhancer as third argument.
   // See https://github.com/rackt/redux/releases/tag/v3.1.0
-  console.log(initialState);
+  const loggerMiddleware = createLogger();
 
-  const store = createStore(rootReducer, initialState, window.devToolsExtension());
+  const createStoreWithMiddleware = applyMiddleware(
+  	thunkMiddleware, 
+  	loggerMiddleware
+  )(createStore);
+
+  const store = createStoreWithMiddleware(rootReducer, initialState);
+  //const store = createStore(rootReducer, initialState, window.devToolsExtension());
 
   return store;
 }
